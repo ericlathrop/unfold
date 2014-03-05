1b8de5604aeda38981ce420bb09737b6da223a0f
Eric Lathrop <eric@ericlathrop.com>
2014-03-05 02:16:49 +0000

    Test single file is copied.

--- fscloner_test.js
+++ fscloner_test.js
@@ -1,5 +1,6 @@
 var rewire = require("rewire");
 var fscloner = rewire("./fscloner");
+var assert = require("assert");
 
 var FS = require("fs-mock");
 
@@ -10,10 +11,25 @@ describe("fscloner", function() {
 				var fs = new FS({});
 				fscloner.__set__("fs", fs);
 				try {
-					fscloner.clone("src", "dest", function(src, dest) {});
+					fscloner.clone("/src", "/dest", function(src, dest) {});
 					throw new Error("should throw an exception");
 				} catch(err) {
 				}
+			});
+		});
+		describe("with single file", function() {
+			it("should call the copyFunc with the file", function(done) {
+				var fs = new FS({
+					"src": {
+						"a.txt": "hello world"
+					}
+				});
+				fscloner.__set__("fs", fs);
+				fscloner.clone("/src", "/dest", function(src, dest) {
+					assert.equal("/src/a.txt", src);
+					assert.equal("/dest/a.txt", dest);
+					done();
+				});
 			});
 		});
 	});
